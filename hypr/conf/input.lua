43fbbe8f8a4272c1b0976674c0885293cb52f597
Melike Vurucu <vcs@melikechan.me>
2026-06-22 07:58:48 +0300

    hyprland.keyboard: modularized existing keyboard settings

---
+++ hypr/conf/input.lua
@@ -0,0 +1,21 @@
+-----------------
+----- Input -----
+-----------------
+-- See https://wiki.hypr.land/Configuring/Basics/Variables/#input
+
+hl.config({
+    input = {
+        kb_layout    = "us,tr,de",
+        kb_variant   = ",,neo",
+        kb_model     = "",
+        kb_options   = "grp:win_space_toggle",
+        kb_rules     = "",
+
+        follow_mouse = 1,
+        sensitivity  = 0,
+
+        touchpad     = {
+            natural_scroll = false
+        }
+    }
+})
