6b709271ddd5c03e2aaa6b0268f6684cb4aa699f
Melike Vurucu <vcs@melikechan.me>
2026-06-22 07:58:23 +0300

    hyprland.gestures: modularized existing gestures

---
+++ hypr/conf/gestures.lua
@@ -0,0 +1,11 @@
+--------------------
+----- Gestures -----
+--------------------
+-- See https://wiki.hypr.land/Configuring/Basics/Variables/#gestures
+
+-- Change workspace
+hl.gesture({
+    fingers   = 3,
+    direction = "horizontal",
+    action    = "workspace"
+})
