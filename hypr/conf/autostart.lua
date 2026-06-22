d5cf66c78d902b9a4c2af39be8860615d6aace36
Melike Vurucu <vcs@melikechan.me>
2026-06-22 07:55:30 +0300

    hyprland.autostart: several changes (see details) - Fixed Proton Mail bridge startup. - Added polkit agent. - Added hyprsunset. - Added clipboard history manager.

--- hypr/conf/autostart.lua
+++ hypr/conf/autostart.lua
@@ -1,8 +1,17 @@
 -----------------
---- AUTOSTART ---
+--- Autostart ---
 -----------------
 
 hl.on("hyprland.start", function()
+    -- Environment for xdg-desktop-portal-hyprland
+    hl.exec_cmd("dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP")
+
+    -- Polkit agent
+    hl.exec_cmd("systemctl --user start hyprpolkitagent")
+
+    -- Night light
+    hl.exec_cmd("hyprsunset")
+
     -- Wallpaper
     hl.exec_cmd("hyprpaper")
 
@@ -15,10 +24,14 @@ hl.on("hyprland.start", function()
     -- hypridle
     hl.exec_cmd("hypridle")
 
+    -- Clipboard History Manager
+    hl.exec_cmd(
+        "wl-paste --watch clipvault store --ignore-pattern '^<meta http-equiv=' --min-entry-length 2 --max-entries 200 --max-entry-age 2d")
+
     -- Application Launcher
     hl.exec_cmd("elephant")
     hl.exec_cmd("walker --gapplication-service")
 
     -- Protonmail Bridge
-    hl.exec_cmd("protonmail-bridge --no-window --noninteractive")
+    hl.exec_cmd("protonmail-bridge --no-window")
 end)
